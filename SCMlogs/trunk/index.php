c0d1c2ee4f75b625682f4086f810418ac99bb960
jfiat <jfiat@028750d0-d4d9-0310-9f49-9f29913c7ed0>
2005-11-01 14:37:28 +0000

    use of PHP SESSIONs to keep repo and user info

--- SCMlogs/trunk/index.php
+++ SCMlogs/trunk/index.php
@@ -1,9 +1,17 @@
 <?php
+ini_set ("session.use_trans_sid", 1);
+
+$this_url = $_SERVER['REQUEST_URI'];
 
 ini_set('magic_quotes_runtime', 0);
 ini_set('magic_quotes_gpc', 0);
 
+session_start();
 
+//echo "<pre>"; print_r ($_SERVER); echo "</pre>";
+//echo "<pre>"; print_r ($_SESSION); echo "</pre>";
+
+$username = $_SESSION['user'];
 
 //error_reporting (55);
 
@@ -12,17 +20,36 @@ include "conf/config.inc";
 include "inc/require.inc";
 include "inc/datamanager.inc";
 
+global $SCMLOGS;
+
 	$_POST = $HTTP_POST_VARS ;
 	$_GET = $HTTP_GET_VARS ;
 	$_COOKIE = $HTTP_COOKIE_VARS ;
 
 if (isset($_GET['repo'])) {
-	SCMLogs_set_repository_id ($_GET['repo']);
+	$asked_repo = $_GET['repo'];
+} elseif (isset ($_SESSION['repo'])) {
+	$asked_repo = $_SESSION['repo'];
+}
+if (isset ($asked_repo)) {
+	if ($asked_repo == $SCMLOGS['repository_id']) {
+		$_SESSION['repo'] = Null;
+		unset ($_SESSION['repo']);
+	} else {
+		SCMLogs_set_repository_id ($asked_repo);
+		$_SESSION['repo'] = $SCMLOGS['repository_id'];
+	}
 }
 
 // Cookies
-	if (!isset($user) and isset($_POST['user'])) { $username = $_POST['user']; }
-	if (!isset($user) and isset($_GET['user'])) { $username = $_GET['user']; }
+	$w_user = Null;
+	if (!isset($w_user) and isset($_POST['w_user'])) { $w_user = $_POST['w_user']; }
+	if (!isset($w_user) and isset( $_GET['w_user'])) { $w_user =  $_GET['w_user']; }
+
+	if (isset ($w_user)) { $username= $w_user; }
+
+$_SESSION['user'] = $username;
+
 	/*
 	$cookiename = "SCMlogs";
 	require ($lib_dir."/cookie.inc");
