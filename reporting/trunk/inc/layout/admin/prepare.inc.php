ae3210d5d91fed2d7b362fd0e139d38823e97b7a
jfiat <jfiat@028750d0-d4d9-0310-9f49-9f29913c7ed0>
2007-02-01 15:35:32 +0000

    admin page can send reminder

--- reporting/trunk/inc/layout/admin/prepare.inc.php
+++ reporting/trunk/inc/layout/admin/prepare.inc.php
@@ -27,53 +27,67 @@
 		$task_switch_lock = "switch-lock";
 		$task_reportinglistcreation = "CreateReportingList";
 		$task_send_email = "SendEmail";
+		$task_send_reminder = "SendReminder";
 
 		if (!isset($task)) { $task = $task_nothing; }
 
 		switch ($task) {
 			case $task_send_email:
 				$cmd = sendReportsMailScript(). " " . $weeks[0] . " new ";
-				$DIS_AdminMessage = "Command: $cmd<BR>\n";
-				$DIS_AdminMessage .= "<HR>";
-				$DIS_AdminMessage .= "<PRE>";
+				$DIS_AdminMessage = "Command: $cmd<br/>\n";
+				$DIS_AdminMessage .= "<hr>";
+				$DIS_AdminMessage .= "<pre>";
 
 				ob_start();
 				$result = system ($cmd);
 				$DIS_AdminMessage .= ob_get_contents();
 				ob_end_clean();
 				
-				$DIS_AdminMessage .= "</PRE>";
+				$DIS_AdminMessage .= "</pre>";
+				break;
+			case $task_send_reminder:
+				$cmd = sendReportsMailScript(). " " . $weeks[0] . " remind ";
+				$DIS_AdminMessage = "Command: $cmd<br/>\n";
+				$DIS_AdminMessage .= "<hr>";
+				$DIS_AdminMessage .= "<pre>";
+
+				ob_start();
+				$result = system ($cmd);
+				$DIS_AdminMessage .= ob_get_contents();
+				ob_end_clean();
+				
+				$DIS_AdminMessage .= "</pre>";
 				break;
 			case $task_switch_lock:
-				$DIS_AdminMessage = "Switch Locks<BR>\n";
+				$DIS_AdminMessage = "Switch Locks<br/>\n";
 				while (list ($key, $val) = each ($weeks)) {
 					$week = $val;
 					if (isLocked ($year, $week)) {
-						$DIS_AdminMessage .= "Task [$task]: UnLock of Reporting $week of $year ...<BR>\n";
+						$DIS_AdminMessage .= "Task [$task]: UnLock of Reporting $week of $year ...<br/>\n";
 
 						ob_start();
 						unLockWeekFilesFor ($year, $week) ;
 						$DIS_AdminMessage .= ob_get_contents();
 						ob_end_clean();
 
-						$DIS_AdminMessage .= "File for week $week of the year $year unLocked<BR>\n";
+						$DIS_AdminMessage .= "File for week $week of the year $year unLocked<br/>\n";
 					} else {
-						$DIS_AdminMessage .= "Task [$task]: lock of Reporting $week of $year ...<BR>\n";
+						$DIS_AdminMessage .= "Task [$task]: lock of Reporting $week of $year ...<br/>\n";
 
 						ob_start();
 						lockWeekFilesFor ($year, $week) ;
 						$DIS_AdminMessage .= ob_get_contents();
 						ob_end_clean();
 
-						$DIS_AdminMessage .= "File for week $week of the year $year locked<BR>\n";
+						$DIS_AdminMessage .= "File for week $week of the year $year locked<br/>\n";
 					}
 				}
 				break;
 			case $task_refresh:
-				$DIS_AdminMessage = "Refreshing<BR>\n";
+				$DIS_AdminMessage = "Refreshing<br/>\n";
 				while (list ($key, $val) = each ($weeks)) {
 					$week = $val;
-					$DIS_AdminMessage .= "Task [$task]: creation of Reporting $week of $year ...<BR>\n";
+					$DIS_AdminMessage .= "Task [$task]: creation of Reporting $week of $year ...<br/>\n";
 
 					ob_start();
 					makeReportingWeekFileFor ($year, $week) ;
@@ -82,7 +96,7 @@
 
 					$urlname = weekUrl ($year, $week);
 					$fname = weekUrl ($year, $week);
-					$DIS_AdminMessage .= "<A HREF='$urlname'>$urlname refreshed</A><BR>\n";
+					$DIS_AdminMessage .= "<a href='$urlname'>$urlname refreshed</a><br/>\n";
 				}
 				break;
 			case $task_reportinglistcreation:
@@ -92,7 +106,7 @@
 				ob_end_clean();
 				break;
 			case $task_delete:
-				$DIS_AdminMessage = "Removing ...<BR>\n";
+				$DIS_AdminMessage = "Removing ...<br/>\n";
 				while (list ($key, $val) = each ($weeks)) {
 					$week = $val;
 					ob_start();
@@ -108,10 +122,10 @@
 				}
 				break;
 			case $task_nothing:
-				$DIS_AdminMessage = "Be careful with what you do ...<BR>\n";
+				$DIS_AdminMessage = "Be careful with what you do ...<br/>\n";
 				break;
 			default:
-				$DIS_AdminMessage = "Task [$task] not yet implemented ...<BR>\n";
+				$DIS_AdminMessage = "Task [$task] not yet implemented ...<br/>\n";
 		}
 
 		$DIS_Weeks = array ();
--- reporting/trunk/tpl/admin.tpl
+++ reporting/trunk/tpl/admin.tpl
@@ -1,60 +1,61 @@
-<TABLE width='100%' border='0' cellpadding=5 cellspacing=2 >
-<TR><TD style="color: Blue;">
-	<TABLE border=1  cellpadding=5 cellspacing=0 width="100%">
-	<TR>
-	<TD>
+<table width='100%' border='0' cellpadding=5 cellspacing=2 >
+<tr><td style="color: Blue;">
+	<table border=1  cellpadding=5 cellspacing=0 width="100%">
+	<tr>
+	<td>
 		{$VAR_ADMIN_MESSAGE}
-	</TD>
-	</TR>
-	</TABLE>
-</TD></TR>
-<TR><TD>
-	<TABLE border=1  cellpadding=5 cellspacing=0 width="100%">
-	<TR>
-	<TD align="center" >
-		<H2>Send the week email</H2> <FORM ACTION='admin.php' METHOD='POST'>
-		<INPUT TYPE='hidden' NAME='admin' VALUE='{$VAR_ADMIN_PASSWORD}'/>
-		<INPUT TYPE='hidden' NAME='task' VALUE='SendEmail' />
+	</td>
+	</tr>
+	</table>
+</td></tr>
+<tr><td>
+	<table border=1  cellpadding=5 cellspacing=0 width="100%">
+	<tr>
+	<td align="center" >
+		<h2>Emailing ...</h2> <form action='admin.php' method='POST'>
+		<input TYPE='hidden' name='admin' value='{$VAR_ADMIN_PASSWORD}'/>
+		<input TYPE='hidden' name='task' value='SendEmail' />
 		Year <input type='text' name='year' value='{$VAR_ADMIN_YEAR}'/>
 		Week <input type='text' name='weeks[]' value='{$VAR_ADMIN_SEND_WEEK}'/>
-		<input type='submit'  value='Send Email' />
-		</FORM>
-	</TD>
+		<input type='submit' name="task" value='SendEmail' />
+		<input type='submit' name="task" value='SendReminder' />
+		</form>
+	</td>
 
-	<TD align="center" >
-		<H2>Create the full Reports list</H2> <FORM ACTION='admin.php' METHOD='POST'>
-		<INPUT TYPE='hidden' NAME='admin' VALUE='{$VAR_ADMIN_PASSWORD}'/>
-		<INPUT TYPE='hidden' NAME='task' VALUE='CreateReportingList' />
+	<td align="center" >
+		<h2>Create the full Reports list</h2> <form action='admin.php' method='POST'>
+		<input TYPE='hidden' name='admin' value='{$VAR_ADMIN_PASSWORD}'/>
+		<input TYPE='hidden' name='task' value='CreateReportingList' />
 		<input type='submit'  value='Create List' />
-		</FORM>
-	</TD></TR>
-	<TR><TD COLSPAN=2 >
+		</form>
+	</td></tr>
+	<tr><td COLSPAN=2 >
 		<h2>Year {$VAR_ADMIN_YEAR}</h2>
-		<FORM ACTION='admin.php' METHOD='POST'>
-		<INPUT TYPE='hidden' NAME='admin' VALUE='{$VAR_ADMIN_PASSWORD}'/>
+		<form action='admin.php' method='POST'>
+		<input TYPE='hidden' name='admin' value='{$VAR_ADMIN_PASSWORD}'/>
 		<input type='hidden' name='year' value='{$VAR_ADMIN_YEAR}'/>
 
 		{foreach from=$ListWeeks item=weekItem key=weekKey}
-		<INPUT TYPE='checkbox' NAME='weeks[]' value='{$weekKey}'>week {$weekKey}  
-		 - <STRONG>{$weekItem.status}</STRONG></INPUT><BR>
+		<input TYPE='checkbox' name='weeks[]' value='{$weekKey}'>week {$weekKey}  
+		 - <strong>{$weekItem.status}</strong></input><br/>
 		{/foreach}
 
 		<input type='submit' name='task' value='delete'/>
 		<input type='submit' name='task' value='refresh'/>
 		<input type='submit' name='task' value='switch-lock'/>
-		</FORM>
-	</TD>
-	</TR>
-	</TABLE>
+		</form>
+	</td>
+	</tr>
+	</table>
 
-</TD>
-<TD width='1%' bgcolor='#000000'>
-	<FORM ACTION='admin.php' METHOD='POST'>
-	<INPUT TYPE='hidden' NAME='admin' VALUE='{$VAR_ADMIN_PASSWORD}'/>
+</td>
+<td width='1%' bgcolor='#000000'>
+	<form action='admin.php' method='POST'>
+	<input TYPE='hidden' name='admin' value='{$VAR_ADMIN_PASSWORD}'/>
 	{foreach from=$ListYears item=yearItem key=yearKey}
-	<INPUT type='submit' name='year' value='{$yearKey}'><BR>
+	<input type='submit' name='year' value='{$yearKey}'><br/>
 	{/foreach}
-	</FORM>
-</TD>
-</TR>
-</TABLE>
+	</form>
+</td>
+</tr>
+</table>
